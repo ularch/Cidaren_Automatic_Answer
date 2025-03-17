cdffcc695184f5fe31b80aa2bfa3d995d7c8097c
ularch <lq13193864059@163.com>
2025-03-17 16:24:00 +0800

    fix(api): 修复了获取更新失败导致的异常退出问题

--- api/update.py
+++ api/update.py
@@ -3,24 +3,32 @@ import json
 
 from log.log import Log
 
-response = requests.get("https://api.github.com/repos/ularch/Cidaren_Automatic_Answer/releases/latest")
-
 update = Log("update")
 
 
 def get_update() -> str:
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data["tag_name"]
-    else:
+    try:
+        response = requests.get("https://api.github.com/repos/ularch/Cidaren_Automatic_Answer/releases/latest")
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data["tag_name"]
+        else:
+            update.logger.info("获取更新失败")
+            return '0'
+    except:
         update.logger.info("获取更新失败")
-        return "null"
+        return '0'
 
 
 def get_update_detail() -> str:
-    if response.status_code == 200:
-        data = json.loads(response.text)
-        return data["body"]
-    else:
+    try:
+        response = requests.get("https://api.github.com/repos/ularch/Cidaren_Automatic_Answer/releases/latest")
+        if response.status_code == 200:
+            data = json.loads(response.text)
+            return data["body"]
+        else:
+            update.logger.info("获取更新内容失败")
+            return 'null'
+    except:
         update.logger.info("获取更新内容失败")
-        return "null"
+        return 'null'
